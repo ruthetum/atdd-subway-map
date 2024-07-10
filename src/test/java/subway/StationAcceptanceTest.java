68611f3cf1cb34683728cf7e243f0ecded5ce7a7
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-07-10 23:18:39 +0900

    [step1-2] step1-1 피드백 반영

--- src/test/java/subway/StationAcceptanceTest.java
+++ src/test/java/subway/StationAcceptanceTest.java
@@ -26,17 +26,16 @@ public class StationAcceptanceTest extends ApiTest {
     @Test
     void createStation() {
         // when
-        String stationName = "강남역";
-        StationRequest request = 지하철역_생성_요청(stationName);
-        ExtractableResponse<Response> createStationResponse = 지하철역_생성(request);
+        var 강남역_생성_요청 = 지하철역_생성_요청("강남역");
+        var 강남역  = 지하철역_생성(강남역_생성_요청);
 
         // then
-        assertThat(createStationResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(강남역.statusCode()).isEqualTo(HttpStatus.CREATED.value());
 
         // then
-        ExtractableResponse<Response> showStationsResponse = 지하철역_조회();
-        List<String> stationNames = showStationsResponse.jsonPath().getList("name", String.class);
-        assertThat(stationNames).containsAnyOf(stationName);
+        var 지하철역_목록 = 지하철역_조회();
+        var 지하철역_이름_목록 = 지하철역_목록.jsonPath().getList("name", String.class);
+        assertThat(지하철역_이름_목록).containsAnyOf("강남역");
     }
 
     /**
@@ -48,19 +47,16 @@ public class StationAcceptanceTest extends ApiTest {
     @Test
     void showStations() {
         // given: 2개의 지하철역 생성
-        String stationName1 = "강남역";
-        지하철역_생성(지하철역_생성_요청(stationName1));
-
-        String stationName2 = "역삼역";
-        지하철역_생성(지하철역_생성_요청(stationName2));
+        지하철역_생성(지하철역_생성_요청("강남역"));
+        지하철역_생성(지하철역_생성_요청("역삼역"));
 
         // when: 지하철역 목록 조회
-        ExtractableResponse<Response> response = 지하철역_조회();
+        var 지하철역_목록 = 지하철역_조회();
 
         // then: 생성되어 있는 2개의 지하철역 응답
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        List<String> stationNames = response.jsonPath().getList("name", String.class);
-        assertThat(stationNames).contains(stationName1, stationName2);
+        assertThat(지하철역_목록.statusCode()).isEqualTo(HttpStatus.OK.value());
+        var 지하철역_이름_목록 = 지하철역_목록.jsonPath().getList("name", String.class);
+        assertThat(지하철역_이름_목록).contains("강남역", "역삼역");
     }
 
     /**
@@ -72,16 +68,16 @@ public class StationAcceptanceTest extends ApiTest {
     @Test
     void deleteStation() {
         // given: 지하철역 생성
-        String stationName = "강남역";
-        ExtractableResponse<Response> createStationResponse = 지하철역_생성(지하철역_생성_요청(stationName));
-        Long createdStationId = createStationResponse.jsonPath().getLong("id");
+        var 강남역 = 지하철역_생성(지하철역_생성_요청("강남역"));
+        var 강남역_ID = 강남역.jsonPath().getLong("id");
 
         // when: 지하철역 제거
-        ExtractableResponse<Response>  deleteStationResponse = 지하철역_제거(createdStationId);
+        var 강남역_제거 = 지하철역_제거(강남역_ID);
 
         // then: 지하철역 목록 조회 시 생성한 역을 찾을 수 없다
-        assertThat(deleteStationResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-        ExtractableResponse<Response> showStationsResponse = 지하철역_조회();
-        assertThat(showStationsResponse.jsonPath().getList("name", String.class)).doesNotContain(stationName);
+        assertThat(강남역_제거.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
+        var 지하철역_목록 = 지하철역_조회();
+        var 지하철역_이름_목록 = 지하철역_목록.jsonPath().getList("name", String.class);
+        assertThat(지하철역_이름_목록).doesNotContain("강남역");
     }
 }
