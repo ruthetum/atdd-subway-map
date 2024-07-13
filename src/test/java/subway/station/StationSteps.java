2ae9381648eb6ebbf572fb2d900950f36d254a92
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-07-13 23:28:09 +0900

    [step1-2] style. 패키지 최적화

--- src/test/java/subway/station/StationSteps.java
+++ src/test/java/subway/station/StationSteps.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
-import subway.station.StationRequest;
 
 public class StationSteps {
 
